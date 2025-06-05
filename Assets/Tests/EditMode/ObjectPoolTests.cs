c47764715d16ff48335644b694850cca80d1444f
Austin Boone <84555510+AusBoone@users.noreply.github.com>
2025-06-05 00:19:26 -0400

    Add basic EditMode tests

---
+++ Assets/Tests/EditMode/ObjectPoolTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using UnityEngine;
+
+// EditMode tests can be run through Unity's Test Runner window (Window > General > Test Runner).
+public class ObjectPoolTests
+{
+    [Test]
+    public void GetAndReturn_ReusesInstance()
+    {
+        var poolGO = new GameObject("pool");
+        var pool = poolGO.AddComponent<ObjectPool>();
+        pool.prefab = new GameObject("prefab");
+
+        var first = pool.GetObject(Vector3.zero, Quaternion.identity);
+        pool.ReturnObject(first);
+        var second = pool.GetObject(Vector3.one, Quaternion.identity);
+
+        Assert.AreSame(first, second);
+        Object.DestroyImmediate(first);
+        Object.DestroyImmediate(pool.prefab);
+        Object.DestroyImmediate(poolGO);
+    }
+}
